53d0ba0c9ab3b584b4128744884f41546e43ae1d
Akito <los24ak@gmail.com>
2022-10-18 17:29:47 +0900

    アプリの設定

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,13 @@ Bundler.require(*Rails.groups)
 
 module PrefectureQuiz
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
