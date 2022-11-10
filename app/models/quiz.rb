13377d6d28a3bf63da7048c8ff36164fc001135a
Akito <los24ak@gmail.com>
2022-11-10 22:33:12 +0900

    Add:クイズと選択肢のアソシエーション

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,6 +2,7 @@ class Quiz < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :played_quizzes
+  has_many :choices
   has_one :prefecture
   mount_uploader :photo, PhotoUploader
 end
