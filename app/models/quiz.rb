a5ed0af6d838ee207e9df5ce4740ae202e3256a8
Akito <los24ak@gmail.com>
2022-11-11 23:45:54 +0900

    Add:アソシエーション追加

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,6 +3,7 @@ class Quiz < ApplicationRecord
   has_many :likes
   has_many :played_quizzes
   has_many :choices
+  accepts_nested_attributes_for :choices, allow_destroy: true
   has_one :prefecture
   mount_uploader :photo, PhotoUploader
 end
