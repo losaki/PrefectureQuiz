b306cf18348d28690ad91c75792e1c2bd224a7cc
Akito <los24ak@gmail.com>
2022-11-16 23:12:50 +0900

    Add:クイズ結果画面仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   resources :users
   resources :quizzes do
-    get 'result', to: 'quiz#result'
+    get 'result', to: 'quizzes#result'
   end
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
