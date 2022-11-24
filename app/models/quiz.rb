303cf6690aa78ce5db9782342d421961845519b1
Akito <los24ak@gmail.com>
2022-11-24 15:33:38 +0900

    Add:dependentオプションの追加

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,8 +1,8 @@
 class Quiz < ApplicationRecord
   belongs_to :user
-  has_many :likes
-  has_many :played_quizzes
-  has_many :choices
+  has_many :likes, dependent: :destroy
+  has_many :played_quizzes, dependent: :destroy
+  has_many :choices, dependent: :destroy
   accepts_nested_attributes_for :choices, allow_destroy: true
   has_one :prefecture
   mount_uploader :photo, PhotoUploader
