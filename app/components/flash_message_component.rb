eed6385694a67ec2ccb543de6f81bcab29029e9c
Akito <los24ak@gmail.com>
2022-11-29 23:44:49 +0900

    Add:フラッシュメッセージの色の出し分け

--- app/components/flash_message_component.rb
+++ app/components/flash_message_component.rb
@@ -4,9 +4,23 @@ class FlashMessageComponent < ViewComponent::Base
   def initialize(type:, data:)
     @type = type
     @data = prepare_data(data)
+    @color = alert_color(type)
   end
 
   private 
+
+  def alert_color(type)
+    case type
+    when "success"
+      "bg-green-100 border-green-500 text-green-700"
+    when "error"
+      "bg-orange-100 border-orange-500 text-orange-700"
+    when "notice"
+      "bg-yellow-100 border-yellow-500 text-yellow-700"
+    else
+      "bg-blue-100 border-blue-500 text-blue-700"
+    end
+  end
 
   def prepare_data(data)
     case data
