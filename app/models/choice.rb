e75192ddc1cf92adb34748d7d82ee50e2f0ecbd7
Akito <los24ak@gmail.com>
2022-12-01 16:38:19 +0900

    Add:バリデーション追加

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,4 +1,6 @@
 class Choice < ApplicationRecord
   belongs_to :quiz
   has_one :prefecture
+  
+  validates :prefecture_id, presence: true
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -6,4 +6,10 @@ class Quiz < ApplicationRecord
   accepts_nested_attributes_for :choices, allow_destroy: true
   has_one :prefecture
   mount_uploader :photo, PhotoUploader
+
+  validates :photo, presence: true
+  validates :prefecture_id, presence: true
+  validates :hint, length: { maximum: 99 }
+  validates :description, length: { maximum: 99 }
+  
 end
