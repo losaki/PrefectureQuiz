5b80fcbe314436604fffdbdb63d1262c14775cb3
Akito <los24ak@gmail.com>
2022-12-11 17:26:20 +0900

    Update:モーダル表示のロジック変更

---
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  hideModal() {
+    this.element.parentElement.removeAttribute("src") // it might be nice to also remove the modal SRC
+    this.element.remove()
+  }
+
+  submitEnd(e) {
+    if (e.detail.success) {
+      this.hideModal()
+    }
+  }
+}
