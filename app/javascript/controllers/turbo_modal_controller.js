9cd51c7a218cc7dcca0586ff6a7a91983657fff3
Akito <los24ak@gmail.com>
2022-12-11 18:04:20 +0900

    Add:モーダル外をクリック時解除する機能

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["modal"]
+  
   hideModal() {
     this.element.parentElement.removeAttribute("src") // it might be nice to also remove the modal SRC
     this.element.remove()
@@ -10,5 +12,17 @@ export default class extends Controller {
     if (e.detail.success) {
       this.hideModal()
     }
+  }
+  closeWithKeyboard(e) {
+    if (e.code == "Escape") {
+      this.hideModal()
+    }
+  }
+
+  closeBackground(e) {
+    if (e && this.modalTarget.contains(e.target)) {
+      return
+    }
+    this.hideModal()
   }
 }
