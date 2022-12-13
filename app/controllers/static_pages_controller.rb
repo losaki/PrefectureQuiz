625d792572921367a079bc1f98b9c8d33b305c2f
Akito <los24ak@gmail.com>
2022-12-13 22:11:36 +0900

    Add:利用規約・プライバシーポリシー・お問い合わせ画面の作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
-  def top
-  end
+  def top; end
+
+  def terms_of_service; end
+
+  def privacy_policy; end
+
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   post 'quizzes/upload_photo', to: 'quizzes#upload_photo'
+  get 'terms_of_service', to: 'static_pages#terms_of_service'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get 'contact', to: 'static_pages#contact'
 end
