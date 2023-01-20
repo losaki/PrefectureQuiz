1e87f04924c56ba57cb08185058ec4ce37ab4138
Akito <los24ak@gmail.com>
2023-01-20 23:10:38 +0900

    Update:meta_tagsを用いた共通化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,17 @@ module ApplicationHelper
     base_title = 'PrefectureQuiz'
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def default_meta_tags
+    {
+      og: {
+        title: 'PrefectureQuiz',
+        description: 'この写真、どこの都道府県でしょう？日本の風景の写真からどこの都道府県かを当てるクイズアプリです。',
+        image: 'https://raw.githubusercontent.com/losaki/PrefectureQuiz/main/app/assets/images/service_image.jpeg'
+      },
+      twitter: {
+        card: 'summary_large_image'
+      }
+    }
+  end
 end
