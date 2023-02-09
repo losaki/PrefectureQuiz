8a0649aa2aa826932ffd70ba7d62cf0be23e8fbb
Akito <los24ak@gmail.com>
2023-02-10 00:44:16 +0900

    Update:users_controllerのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,31 +8,31 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, success: "ユーザー登録が完了しました"
+      redirect_to login_path, success: 'ユーザー登録が完了しました'
     else
-      flash.now[:notice] = "ユーザー登録に失敗しました"
+      flash.now[:notice] = 'ユーザー登録に失敗しました'
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def destroy
     @user.destroy!
-    redirect_to root_path, success: "ユーザー情報を削除しました"
+    redirect_to root_path, success: 'ユーザー情報を削除しました'
   end
 
   def update
     if @user.update(user_params)
-      redirect_to user_path, success: "ユーザー情報を更新しました"
+      redirect_to user_path, success: 'ユーザー情報を更新しました'
     else
-      flash.now[:notice] = "ユーザー情報の更新に失敗しました"
+      flash.now[:notice] = 'ユーザー情報の更新に失敗しました'
       render :edit, status: :unprocessable_entity
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
