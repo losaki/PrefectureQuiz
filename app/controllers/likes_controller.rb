061341e16d77ac06bd3f53523a60ec73949b0f53
Akito <los24ak@gmail.com>
2023-03-01 18:22:01 +0900

    Add:いいね機能のturbo化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,16 @@ class LikesController < ApplicationController
   def create
     quiz = Quiz.find(params[:quiz_id])
     current_user.like(quiz)
-    redirect_back fallback_location: root_path
+    render turbo_stream: turbo_stream.replace(
+      'like-button', partial: 'quizzes/like_button', locals: { quiz: quiz }
+    )
   end
 
   def destroy
     quiz = current_user.likes.find_by(quiz_id: params[:id]).quiz
     current_user.unlike(quiz)
-    redirect_back fallback_location: root_path
+    render turbo_stream: turbo_stream.replace(
+      'like-button', partial: 'quizzes/like_button', locals: { quiz: quiz }
+    )
   end
 end
