23aa2d1598902db2a4bd0e7d5d2ddab19e9ba413
Akito <los24ak@gmail.com>
2023-06-20 16:05:22 +0900

    Delete:クイズ並び替え機能：不要箇所の削除

--- app/javascript/sort_select.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const sortSelect = document.querySelector('.sort-select');
-  sortSelect.addEventListener('change', function() {
-    this.closest('form').submit();
-  });
-});
